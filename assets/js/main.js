efce8b9875c9f40249e192d80883319229fef19e
MoSuperCode <mo.supercode@gmail.com>
2022-11-14 15:34:19 +0100

    Abgabe

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." }
+];
+
+
+function encrypt() {
+    let inputElement = document.getElementById("inputElement").value;
+    let inputCap = inputElement.toUpperCase();
+    let outputElement = document.getElementById("outputElement");
+    let array = [];
+    let joinedArray;
+
+    for (let letter of inputCap) {
+        for (let morseLetter of morseAlphabet) {
+
+            if (letter === morseLetter.letter) {
+                array.push(morseLetter.morseCode);
+                joinedArray = array.join(" ");
+            }
+        }
+
+    }
+    outputElement.innerHTML = joinedArray;
+}
